1150dd1ade79a3927ca26ef0958b60be1a55043b
John Kloke <johnkloke@gmail.com>
2014-12-12 10:51:17 -0600

    fix test-convergence.r

--- tests/testthat/test-convergence.r
+++ tests/testthat/test-convergence.r
@@ -3,7 +3,7 @@ EPS <- sqrt(.Machine$double.eps)
 test_that("rfit convergence", {
   x<-y<-rep(0:1,each=10)
   err<-rfit(y~x)$coef[2]-1
-  expect_less_than(err,1+EPS)
+  expect_less_than(err,EPS)
 })
 
 
